3e87d1d8a079db28ffcf8100f800fa3d6673ee75
NISHI3 <ryonishi3@gmail.com>
2018-05-14 02:19:33 +0900

    remove unused code

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,14 +1,8 @@
-// fetch ({ store, redirect }) {
-//     if (!store.state.authUser) {
-//       return redirect('/')
-//     }
-//   }
-
-  export default function({store, redirect}) {
-    if(!store.state.authUser) {
-      return redirect('/');
-    }
-    else {
-      return redirect('/hoge');
-    }
+export default function({store, redirect}) {
+  if(!store.state.authUser) {
+    return redirect('/');
   }
+  else {
+    return redirect('/hoge');
+  }
+}
