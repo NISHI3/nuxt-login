38b1ef12f0a8723145b30fb1c64d3e1017980ddf
NISHI3 <ryonishi3@gmail.com>
2018-05-14 02:04:49 +0900

    update pages /

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,22 @@
 <template>
-  <section class="hero">
-    <div class="hero-body">
-      <div class="container has-text-centered">
-        <div class="column is-4 is-offset-4">
-          <div class="box">
-            <h3 class="title has-text-grey">Login</h3>
-            <form @submit.prevent="login">
-              <div class="field">
-                <div class="control">
-                  <input class="input is-large" type="text" v-model="formUsername" name="username" placeholder="Username" autofocus="">
-                </div>
-              </div>
-
-              <div class="field">
-                <div class="control">
-                  <input class="input is-large" type="password" v-model="formPassword" name="password" placeholder="Password">
-                </div>
-              </div>
-              <button class="button is-block is-info is-large is-fullwidth" type="submit">Login</button>
-            </form>
-            <button class="button" @click="logout">Logout</button>
-          </div>
-        </div>
-      </div>
+  <div class="hero-body">
+    <div class="container has-text-centered">
+      <h1 class="title">
+        Title
+      </h1>
+      <h2 class="subtitle">
+        Subtitle
+      </h2>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
 export default {
-  layout: 'index',
-  data() {
-    return {
-      formError: null,
-      formUsername: '',
-      formPassword: ''
-    }
-  },
-  methods: {
-    async login() {
-      try {
-        await this.$store.dispatch('login', {
-          username: this.formUsername,
-          password: this.formPassword
-        })
-        this.formUsername = ''
-        this.formPassword = ''
-        this.formError = null
-      } catch (e) {
-        this.formError = e.message
-      }
-    },
-    async logout() {
-      try {
-        await this.$store.dispatch('logout')
-      } catch (e) {
-        this.formError = e.message
-      }
-    }
-  }
+  layout: 'index'
 }
 </script>
 
 <style>
-.container {
-  padding: 100px;
-}
-.error {
-  color: red;
-}
+
 </style>
