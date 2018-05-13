b74d292dde598756fb03ff022cf1e57892a27434
NISHI3 <ryonishi3@gmail.com>
2018-05-13 21:07:01 +0900

    add pages

---
+++ pages/secret/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Super secret page</h1>
+    <router-link to="/">Back to the home page</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: 'auth'
+}
+</script>
