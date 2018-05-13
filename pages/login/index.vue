709b86008a745d9d240d59a651d20dc7f2bb8958
NISHI3 <ryonishi3@gmail.com>
2018-05-14 02:04:33 +0900

    move pages / => /login

---
+++ pages/login/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <section class="hero">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <div class="column is-4 is-offset-4">
+          <div class="box">
+            <h3 class="title has-text-grey">Login</h3>
+            <form @submit.prevent="login">
+              <p class="error" v-if="formError">{{ formError }}</p>
+              <div class="field">
+                <div class="control">
+                  <input class="input is-large" type="text" v-model="formUsername" name="username" placeholder="Username" autofocus="">
+                </div>
+              </div>
+
+              <div class="field">
+                <div class="control">
+                  <input class="input is-large" type="password" v-model="formPassword" name="password" placeholder="Password">
+                </div>
+              </div>
+              <button class="button is-block is-info is-large is-fullwidth" type="submit">Login</button>
+            </form>
+            <button class="button" @click="logout">Logout</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formError: null,
+      formUsername: '',
+      formPassword: ''
+    }
+  },
+  methods: {
+    async login() {
+      try {
+        await this.$store.dispatch('login', {
+          username: this.formUsername,
+          password: this.formPassword
+        })
+        this.formUsername = ''
+        this.formPassword = ''
+        this.formError = null
+      } catch (e) {
+        this.formError = e.message
+      }
+    },
+    async logout() {
+      try {
+        await this.$store.dispatch('logout')
+      } catch (e) {
+        this.formError = e.message
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  .container {
+    padding: 100px;
+  }
+  .error {
+    color: red;
+  }
+</style>
+
