9751a06339185157074bc13675cf53cfa2d81ff2
NISHI3 <ryonishi3@gmail.com>
2018-05-13 21:07:45 +0900

    add store

---
+++ store/index.js
@@ -0,0 +1,64 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+// window.fetch() のためのポリフィル
+require('whatwg-fetch')
+
+const store = () => new Vuex.Store({
+
+  state: {
+    authUser: null
+  },
+
+  mutations: {
+    SET_USER: function (state, user) {
+      state.authUser = user
+    }
+  },
+
+  actions: {
+    nuxtServerInit ({ commit }, { req }) {
+      if (req.session && req.session.authUser) {
+        commit('SET_USER', req.session.authUser)
+      }
+    },
+    login ({ commit }, { username, password }) {
+      return fetch('/api/login', {
+        // クライアントのクッキーをサーバーに送信
+        credentials: 'same-origin',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username,
+          password
+        })
+      })
+      .then((res) => {
+        if (res.status === 401) {
+          throw new Error('Bad credentials')
+        } else {
+          return res.json()
+        }
+      })
+      .then((authUser) => {
+        commit('SET_USER', authUser)
+      })
+    },
+    logout ({ commit }) {
+      return fetch('/api/logout', {
+        // クライアントのクッキーをサーバーに送信
+        credentials: 'same-origin',
+        method: 'POST'
+      })
+      .then(() => {
+        commit('SET_USER', null)
+      })
+    }
+  }
+})
+
+export default store
